58a9755ec169c2d7edec77f9f1bc55a934acefd2
Oleg Cherednik <oleg.cherednik@xyleme.com>
2015-07-30 17:07:40 +0300

    Bug #1: Icon 48x48 bit 8 is shown incorrectly

--- src/main/java/cop/swing/icoman/bitmap/BitmapFix.java
+++ src/main/java/cop/swing/icoman/bitmap/BitmapFix.java
@@ -15,6 +15,100 @@ public final class BitmapFix extends Bitmap {
         readImage(in);
     }
 
+    private void bitLess8(BitmapInfoHeader header, byte[] buf, Color[] colorTable, byte[] bitMasks, byte[] data, BufferedImage bufferedImage) {
+        int bitCount = header.getBiBitCount();
+        int n;
+        int n2 = 0;
+        int n3 = width;
+        int n4 = data.length / height;
+        int n5 = 0;
+        int n6 = 0;
+        int n7;
+        int n8;
+        int[] buf1 = new int[width * height];
+        if (bitCount == 8) {
+            if (bitMasks.length == buf1.length) {
+                for (n8 = 0; n8 < buf1.length; ++n8) {
+                    buf1[n8] = bitMasks[n8] & 255;
+                }
+            } else {
+                n2 = 0;
+                n4 = n3;
+                n5 = 0;
+                n6 = bitMasks.length / height - width;
+                for (n8 = 0; n8 < bitMasks.length; ++n8) {
+                    buf1[n2++] = bitMasks[n8] & 255;
+                    if (++n5 != n4) continue;
+                    n5 = 0;
+                    n8 += n6;
+                }
+            }
+        } else if (bitCount == 4) {
+            n = 0;
+            if (bitMasks.length * 2 == buf1.length) {
+                for (n8 = 0; n8 < bitMasks.length; ++n8) {
+                    buf1[n++] = (bitMasks[n8] & 255) >> 4;
+                    buf1[n++] = (bitMasks[n8] & 255) >> 4 << 4 ^ bitMasks[n8] & 255;
+                }
+            } else {
+                n2 = 0;
+                n4 = n3;
+                n5 = 0;
+                n6 = bitMasks.length * 2 / height - width;
+                n8 = 1;
+                for (n7 = 0; n7 < bitMasks.length; ++n7) {
+                    if (n8 != 0) {
+                        buf1[n++] = (bitMasks[n7] & 255) >> 4;
+                    }
+                    if (n8 != 0 && ++n5 == n4) {
+                        n5 = 0;
+                        n8 = 0;
+                    } else if (n8 == 0 && n5 == n6) {
+                        n5 = 0;
+                        n8 = 1;
+                    }
+                    if (n8 != 0) {
+                        buf1[n++] = (bitMasks[n7] & 255) >> 4 << 4 ^ bitMasks[n7] & 255;
+                    }
+                    if (n8 != 0 && ++n5 == n4) {
+                        n5 = 0;
+                        n8 = 0;
+                        continue;
+                    }
+                    if (n8 != 0 || n5 != n6) continue;
+                    n5 = 0;
+                    n8 = 1;
+                }
+            }
+        } else if (bitCount == 1) {
+            n2 = 0;
+            n3 = width;
+            n4 = data.length / height;
+            n5 = 0;
+            n6 = 0;
+            for (n8 = 0; n8 < bitMasks.length; ++n8) {
+                ++n5;
+                for (n7 = 7; n7 >= 0; --n7) {
+                    if (n6 >= n3) continue;
+                    buf1[n2++] = (1 << n7 & bitMasks[n8]) != 0 ? 1 : 0;
+                    ++n6;
+                }
+                if (n5 != n4) continue;
+                n5 = 0;
+                n6 = 0;
+            }
+        }
+        n = 0;
+        for (n7 = height - 1; n7 >= 0; --n7) {
+            for (int j = 0; j < width; ++j) {
+                Color color = new Color(colorTable[buf1[n]].getRed(), colorTable[buf1[n]].getGreen(), colorTable[buf1[n]].getBlue(),
+                        buf[n] & 255);
+                bufferedImage.setRGB(j, n7, color.getRGB());
+                ++n;
+            }
+        }
+    }
+
     // ========== Bitmap ==========
 
     @Override
@@ -24,9 +118,9 @@ public final class BitmapFix extends Bitmap {
         byte[] bitMasks = readBitMasks(header, in);
         byte[] data = readData(header, in);
         int bitCount = header.getBiBitCount();
+        byte[] buf = new byte[width * height];
 
         int n;
-        byte[] buf = new byte[width * height];
         int n2 = 0;
         int n3 = width;
         int n4 = data.length / height;
@@ -45,91 +139,9 @@ public final class BitmapFix extends Bitmap {
         }
         n = 0;
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+
         if (bitCount <= 8) {
-            int n7;
-            int n8;
-            int[] buf1 = new int[width * height];
-            if (bitCount == 8) {
-                if (bitMasks.length == buf1.length) {
-                    for (n8 = 0; n8 < buf1.length; ++n8) {
-                        buf1[n8] = bitMasks[n8] & 255;
-                    }
-                } else {
-                    n2 = 0;
-                    n4 = n3;
-                    n5 = 0;
-                    n6 = bitMasks.length / height - width;
-                    for (n8 = 0; n8 < bitMasks.length; ++n8) {
-                        buf1[n2++] = bitMasks[n8] & 255;
-                        if (++n5 != n4) continue;
-                        n5 = 0;
-                        n8 += n6;
-                    }
-                }
-            } else if (bitCount == 4) {
-                n = 0;
-                if (bitMasks.length * 2 == buf1.length) {
-                    for (n8 = 0; n8 < bitMasks.length; ++n8) {
-                        buf1[n++] = (bitMasks[n8] & 255) >> 4;
-                        buf1[n++] = (bitMasks[n8] & 255) >> 4 << 4 ^ bitMasks[n8] & 255;
-                    }
-                } else {
-                    n2 = 0;
-                    n4 = n3;
-                    n5 = 0;
-                    n6 = bitMasks.length * 2 / height - width;
-                    n8 = 1;
-                    for (n7 = 0; n7 < bitMasks.length; ++n7) {
-                        if (n8 != 0) {
-                            buf1[n++] = (bitMasks[n7] & 255) >> 4;
-                        }
-                        if (n8 != 0 && ++n5 == n4) {
-                            n5 = 0;
-                            n8 = 0;
-                        } else if (n8 == 0 && n5 == n6) {
-                            n5 = 0;
-                            n8 = 1;
-                        }
-                        if (n8 != 0) {
-                            buf1[n++] = (bitMasks[n7] & 255) >> 4 << 4 ^ bitMasks[n7] & 255;
-                        }
-                        if (n8 != 0 && ++n5 == n4) {
-                            n5 = 0;
-                            n8 = 0;
-                            continue;
-                        }
-                        if (n8 != 0 || n5 != n6) continue;
-                        n5 = 0;
-                        n8 = 1;
-                    }
-                }
-            } else if (bitCount == 1) {
-                n2 = 0;
-                n3 = width;
-                n4 = data.length / height;
-                n5 = 0;
-                n6 = 0;
-                for (n8 = 0; n8 < bitMasks.length; ++n8) {
-                    ++n5;
-                    for (n7 = 7; n7 >= 0; --n7) {
-                        if (n6 >= n3) continue;
-                        buf1[n2++] = (1 << n7 & bitMasks[n8]) != 0 ? 1 : 0;
-                        ++n6;
-                    }
-                    if (n5 != n4) continue;
-                    n5 = 0;
-                    n6 = 0;
-                }
-            }
-            n = 0;
-            for (n7 = height - 1; n7 >= 0; --n7) {
-                for (int j = 0; j < width; ++j) {
-                    Color color = new Color(colorTable[buf1[n]].getRed(), colorTable[buf1[n]].getGreen(), colorTable[buf1[n]].getBlue(),
-                            buf[n] & 255);
-                    bufferedImage.setRGB(j, n7, color.getRGB());
-                    ++n;
-                }
-            }
+            bitLess8(header, buf, colorTable, bitMasks, data, bufferedImage);
         } else {
             n = 0;
             if (bitCount == 24) {
